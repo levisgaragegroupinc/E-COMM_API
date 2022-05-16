8e90070a9f39239f77a2bb39a8f4a1711f6e8f57
Travis Bates <tbateswsu@gmail.com>
2022-05-15 17:41:28 -0700

    set up fields and rules for the models, products, producttags, tags, and category files. Installed required packages, express, sequelize, mysql

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,20 +1,69 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
+const Product = require("./Product");
+const Tag = require("./Tag");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      // - References the `Product` model's `id`.
+      references: {
+        model: Product,
+        key: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // - References the `Tag` model's `id`.
+      references: {
+        model: Tag,
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
 module.exports = ProductTag;
+
+// REQUIREMENTS FOR PRODUCT TAGS MODEL
+// - `ProductTag`
+
+//   - `id`
+
+//     - Integer.
+
+//     - Doesn't allow null values.
+
+//     - Set as primary key.
+
+//     - Uses auto increment.
+
+//   - `product_id`
+
+//     - Integer.
+
+//     - References the `Product` model's `id`.
+
+//   - `tag_id`
+
+//     - Integer.
+
+//     - References the `Tag` model's `id`.
