837643639d286b132677d9d1089047d9881bfb04
Travis Bates <tbateswsu@gmail.com>
2022-05-15 22:42:36 -0700

    added delete tag route.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -86,8 +86,23 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!deleteTag) {
+      res.status(404).json({
+        message: `No tag found with id: ${req.params.id}. ID not found.`,
+      });
+    }
+    res.status(200).json(deleteTag);
+  } catch (error) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
