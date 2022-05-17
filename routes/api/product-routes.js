4298994b419746c1a3c5e621116772544e4cefb0
Travis Bates <tbateswsu@gmail.com>
2022-05-16 18:25:10 -0700

    updated create product route. Still need to add the try catch to the route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -57,7 +57,7 @@ router.get("/:id", async (req, res) => {
 
 // CREATE NEW PRODUCT ROUTE: IN-PROGRESS!
 // create new product
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -66,19 +66,21 @@ router.post("/", (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
     */
-  Product.create(req.body, {
+  const createNewProduct = await Product.create(req.body, {
     product_name: req.body.product_name,
     price: req.body.price,
     stock: req.body.stock,
     category_id: req.body.category_id,
     tagIds: req.body.tagIds,
   })
-    .then((product) => {
+    // .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+     
+     
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
-            product_id: product.id,
+            product_id: createNewProduct.id,
             tag_id,
           };
         });
